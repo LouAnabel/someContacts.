7e58ef7bc59ac3ee7b7c6ec6a28f47ac3909b922
LouAnabel <loudebatin@gmail.com>
2025-06-26 18:03:28 +0200

    updated tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+    <div className="min-h-screen bg-linear-to-br from-blue-500 to-purple-600 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-xl">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">
           Hello Tailwind!
