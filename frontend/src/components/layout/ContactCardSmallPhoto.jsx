993c06bddf7fa084719438cdf1c70c9cf0d7471a
LouAnabel <loudebatin@gmail.com>
2025-07-18 16:08:10 +0200

    changed margin left

--- frontend/src/components/layout/ContactCardSmallPhoto.jsx
+++ frontend/src/components/layout/ContactCardSmallPhoto.jsx
@@ -42,7 +42,7 @@ const ContactCardSmallPhoto = ({contact = {}}) => {
                         />
                     </div>
                     {/* Dropdown Menu Button */}
-                    <span >
+                    <span className="-mr-1">
                     <ContactMenuBar 
                         isMenuOpen={isMenuOpen} 
                         setIsMenuOpen={setIsMenuOpen} 
@@ -58,15 +58,15 @@ const ContactCardSmallPhoto = ({contact = {}}) => {
 
                 {/* Contact Details */}
                 <div className="w-full relative justify-items flex flex-col mt-5 mb-4 space-y-1">
-                    <div className="flex items-center justify-between w-full ml-2 mb-1">
+                    <div className="flex items-center justify-between w-full mb-1">
                         {/* City and Country */}
-                        <span className="text-black font-text text-normal font-light">{contact.city}, {contact.country}</span>
+                        <span className="text-black font-text text-normal font-light ml-4">{contact.city}, {contact.country}</span>
                         
                         {/* Favorite Star */}
                         <button
                             type="button"
                             onClick={() => setFormData(prev => ({ ...prev, isFavorite: !prev.isFavorite }))}
-                            className="flex items-center hover:scale-110 transform"
+                            className="flex items-center hover:scale-110 transform -mr-2"
                             aria-label={formData.isFavorite ? "Unmark as favorite" : "Mark as favorite"}
                         >
                             <svg 
@@ -82,13 +82,13 @@ const ContactCardSmallPhoto = ({contact = {}}) => {
                             </svg>
                         </button>
                     </div>
-                    <div class="relative left flex items-center pointer-events-none ml-2 dark:text-black">
+                    <div class="relative left flex items-center pointer-events-none ml-4 dark:text-black">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                         </svg>
                         <span className="ml-4 text-blue-800 text-medium font-light">{contact.phone}</span>
                     </div>
-                    <div class="relative flex items-center pointer-events-none ml-2 dark:text-black">
+                    <div class="relative flex items-center pointer-events-none ml-4 dark:text-black">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                         </svg>
